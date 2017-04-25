de0b2f1ca183bd80dddc51625000ab01a383c3de
Michel Bouchou <michel.bouchou@gmail.com>
2017-04-25 18:55:52 +0200

    Modif

--- Peer project/run_analysis.R
+++ Peer project/run_analysis.R
@@ -1,7 +1,7 @@
 run_analysis <- function() {
     
     library("dplyr")
-    
+
     # Get data
     
     train <- read.table("UCI HAR Dataset/train/X_train.txt")
@@ -14,36 +14,33 @@ run_analysis <- function() {
     
     # Merge to one big dataset
     
-    train <- cbind(train_y, train_sub, train)
-    test <- cbind(test_y, test_sub, test)
+    train <- cbind(train_sub, train_y, train)
+    test <- cbind(test_sub, test_y, test)
     merged <- rbind(train, test)
     
     # Name the colum
     
     features <- read.table("UCI HAR Dataset/features.txt")
-    names <- c("Labels", "Subject")
+    names <- c("subject", "labels")
     names <- c(names, as.character(features$V2))
     colnames(merged) <- names
     
     # Get the "mean" and "std" values
     
-    smaller <- merged[grepl("Labels", names(merged)) | grepl("Subject", names(merged)) | grepl("mean", names(merged)) | grepl("std", names(merged))]
+    smaller <- merged[grepl("subject", names(merged)) | grepl("labels", names(merged)) | grepl("mean", names(merged)) | grepl("std", names(merged))]
     
     # Labels the activities
     
-    labelled <- merge(smaller, labels, by.x = "Labels", by.y = "V1")
+    labelled <- merge(smaller, labels, by.x = "labels", by.y = "V1")
     labelled$labels <- labelled$V2
     labelled <- labelled[-82]
-    arrange(labelled, subject, labels)
     
     # Get the second tidy dataset with means value
     
-    mean <- labelled[-c(1, 2)]
-    mean <- as.data.frame(sapply(mean, mean))
+    tidy <- labelled %>% group_by(subject, labels) %>% summarise_each(funs(mean))
     
-    # Write the datasets
+    # Write the dataset
     
-    write.table(labelled, "tidy.txt")
-    write.table(mean, "mean.txt")
+    write.table(tidy, "tidy.txt", row.names = FALSE)
     
 }
